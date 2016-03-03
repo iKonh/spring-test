c4320989ca5016d2c2b218e89b0da1018ae52804
huangyipeng <huangyipeng@smart-tec-cn.com>
2016-03-03 17:35:36 +0800

    redis学习

---
+++ spring-data-redis/src/main/java/redis/Controller/UserController.java
@@ -0,0 +1,16 @@
+package redis.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * Created by Huangyipeng on 2016/03/03.
+ */
+public class UserController {
+
+    @Autowired
+    RedisTemplate template;
+
+//    public
+}
---
+++ spring-data-redis/src/main/java/redis/Dao/UserDao.java
@@ -0,0 +1,10 @@
+package redis.Dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Huangyipeng on 2016/03/03.
+ */
+@Component
+public class UserDao {
+}
